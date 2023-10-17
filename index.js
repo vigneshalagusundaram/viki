dcb4cb9f5227dfd75975a5e3709bbd26592dc22f
a.vignesh05 <a.vignesh05@gmail.com>
2023-10-17 16:35:19 +0530

    env changes

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const app = express()
 const indexRouter =require("./Router/index")
 const usersRouter =require("./Router/users")
 const DeshboardRouter =require("./Router/Deshboard")
-
+const PORT=process.env.PORT || 3100
 app.use(express.json())
 app.use('/',indexRouter)
 app.use('/users',usersRouter)
 app.use('/Deshboard',DeshboardRouter)
 
 
-app.listen(3100,()=>console.log("App running in the port 3100"))
+app.listen(PORT,()=>console.log(`App running in the port ${PORT}`))
